5392cc86db757e2dad5ad2fd34588618cf3795df
JYP <zhsclqzlffj2@gmail.com>
2023-03-02 14:49:26 +0900

    first commit

---
+++ src/main/java/com/example/mapFinalTest/MapFinalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapFinalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapFinalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapFinalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mapFinalTest/MapFinalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapFinalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapFinalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
